ab53e5dd6a15134fb975d0179e3190c9c31e8154
Ryan Augustyn <jraugustyn07@gmail.com>
2023-03-09 16:58:35 -0600

    Finished tutorial videos

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from marshmallow import post_load, fields, ValidationError
 from dotenv import load_dotenv
 from os import environ
 
@@ -23,15 +24,86 @@ CORS(app)
 Migrate(app, db)
 
 # Models
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String(255), nullable=False)
+    model = db.Column(db.String(255), nullable=False)
+    year = db.Column(db.Integer)
 
+    def __repr__(self):
+        return f'{self.year} {self.make} {self.model}'
 
+#flask db migrate -m "Init"-creates folder
+#flask db upgrade-run file and add columns, use to change 
 
 # Schemas
+class CarSchema(ma.Schema):
+    id = fields.Integer(primary_key=True)
+    make = fields.String(required=True)
+    model = fields.String(required=True)
+    year = fields.Integer()
 
+    class Meta:
+        fields = ("id", "make", "model", "year")
+    
+    @post_load
+    def create_car(self, data, **kwargs):
+        return Car(**data)
+
+car_schema = CarSchema()
+cars_schema = CarSchema(many=True)
+
+# my_car = Car(id=3, make ="Toyota", model="Corolla", year =2020)
+# serialized_car = car_schema.dump(my_car)
+# print(serialized_car)
 
 
 # Resources
+class CarListResource(Resource):
+    def get(self):
+        all_cars = Car.query.all()
+        return cars_schema.dump(all_cars)
+    
+    def post(self):
+        form_data = request.get_json()
+
+    #obsolete with create_car method in CarSchema
+        # new_car = Car(
+        #     make=request.json['make'],
+        #     model=request.json['model'],
+        #     year=request.json['year']
+        # )
+        try:
+            new_car = car_schema.load(form_data)
+            db.session.add(new_car)
+            db.session.commit()
+            return car_schema.dump(new_car), 201
+        except ValidationError as err:
+            return err.messages, 400
+
+class CarResource(Resource):
+    def get(self, car_id):
+        car_from_db = Car.query.get_or_404(car_id)
+        return car_schema.dump(car_from_db)
+    
+    def delete(self, car_id):
+        car_from_db = Car.query.get_or_404(car_id)
+        db.session.delete(car_from_db)
+        return '', 204
+    
+    def put(self, car_id):
+        car_from_db = Car.query.get_or_404(car_id)
+        if 'make' in request.json:
+            car_from_db.make = request.json['make']
+        if 'model' in request.json:
+            car_from_db.model = request.json['model']
+        if 'year' in request.json:
+            car_from_db.year = request.json['year']
+        db.session.commit()
+        return car_schema.dump(car_from_db)
 
 
 
 # Routes
+api.add_resource(CarListResource, '/api/cars')
+api.add_resource(CarResource, '/api/cars/<int:car_id>')
